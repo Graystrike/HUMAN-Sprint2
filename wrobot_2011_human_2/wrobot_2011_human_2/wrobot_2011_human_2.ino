7a44d9207782bb0a3a2a8b0cbed5a4e7b1511957
lwhipker <lbbjnews@gmail.com>
2013-11-21 20:24:24 -0500

    second version of code

---
+++ wrobot_2011_human_2/wrobot_2011_human_2/wrobot_2011_human_2.ino
@@ -0,0 +1,46 @@
+// Arduino PWM Speed Control：
+// Pin definitions for PWM speed control using 
+// DFRobot Arduino Compatible Motor Shield (2A):
+
+#include "MotorRobotBase.h"
+
+#define Motor MotorRobotBase
+
+void forward (Motor &m1, Motor &m2) 
+{
+  int speed; 
+  // speed up
+  for (speed = 0; speed <= 255; speed += 5) {
+    m1.forward(50);
+    m2.forward(50);
+    delay(90)
+    ; 
+  }
+  // speed down
+  for (speed = 255; speed >= 0; speed -= 5) {
+    m1.forward(0);
+    m2.forward(0);
+    delay(10)
+    ;
+  }
+  for (speed = 0; speed <= 255; speed += 5) 
+    m1.forward(50);
+    m2.forward(50);
+}
+
+Motor right_motor(Motor::MOTOR_1);
+Motor left_motor(Motor::MOTOR_2);
+
+void setup () 
+{ 
+  right_motor.setup();
+  left_motor.setup();
+} 
+
+void loop () 
+{ 
+  forward(left_motor, right_motor); 
+}
+
+
+
